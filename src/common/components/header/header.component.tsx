bed5d38dce77c2bacaf3a93839a891953813b7dd
Dmit.D <dmit.dy@gmail.com>
2023-01-03 14:59:50 +0200

    add container

--- src/common/components/header/header.component.tsx
+++ src/common/components/header/header.component.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import clsx from 'clsx'
+import { Container } from '../container/container.component'
 
 export const Header: React.FC = () => {
 	const navLinkClasses = ({ isActive }: { isActive: boolean }) =>
@@ -12,31 +13,33 @@ export const Header: React.FC = () => {
 	return (
 		<header>
 			<nav className='px-2 py-4'>
-				<div className='max-w-screen-xl mx-auto flex justify-between items-center'>
-					<Link
-						to='/'
-						className='font-josefin text-2xl mr-8 text-theme-blue font-bold'
-					>
-						Blogtub
-					</Link>
-					<ul className='pl-0 mb-0 list-none flex'>
-						<li>
-							<NavLink to='/' className={navLinkClasses}>
-								Home
-							</NavLink>
-						</li>
-						<li className='ml-4'>
-							<NavLink to='/sign-in' className={navLinkClasses}>
-								Sign in
-							</NavLink>
-						</li>
-						<li className='ml-4'>
-							<NavLink to='/sign-up' className={navLinkClasses}>
-								Sign up
-							</NavLink>
-						</li>
-					</ul>
-				</div>
+				<Container>
+					<div className='flex justify-between items-center'>
+						<Link
+							to='/'
+							className='font-josefin text-2xl mr-8 text-theme-blue font-bold'
+						>
+							Blogtub
+						</Link>
+						<ul className='pl-0 mb-0 list-none flex'>
+							<li>
+								<NavLink to='/' className={navLinkClasses}>
+									Home
+								</NavLink>
+							</li>
+							<li className='ml-4'>
+								<NavLink to='/sign-in' className={navLinkClasses}>
+									Sign in
+								</NavLink>
+							</li>
+							<li className='ml-4'>
+								<NavLink to='/sign-up' className={navLinkClasses}>
+									Sign up
+								</NavLink>
+							</li>
+						</ul>
+					</div>
+				</Container>
 			</nav>
 		</header>
 	)
