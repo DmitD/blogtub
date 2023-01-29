c47c38c3bbd38976e04f333caa93360854da7e5a
Dmit.D <dmit.dy@gmail.com>
2023-01-29 15:01:52 +0200

    add profile

---
+++ client/src/modules/profile/pages/profile.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ProfileBanner } from '../components/profile-banner/profile-banner'
+
+interface ProfilePageProps {}
+
+export const ProfilePage: React.FC = () => {
+	return (
+		<>
+			<ProfileBanner />
+		</>
+	)
+}
