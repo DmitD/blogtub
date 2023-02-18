915526d4962a31263045f2dfe79fe6458ce913fb
Dmit.D <dmit.dy@gmail.com>
2023-02-18 21:23:27 +0200

    add use-auth hook

---
+++ client/src/modules/auth/hooks/use-auth.tsx
@@ -0,0 +1,47 @@
+import { useAppDispatch, useAppSelector } from '../../../store/store'
+import { useLazySignInQuery, useLazySignUpQuery } from '../api/reposritory'
+import { SignInOutDTO } from '../api/dto/sign-in.out'
+import { SignUpOutDTO } from '../api/dto/sign-up.out'
+import { selectUser, setUser } from '../service/auth.slice'
+
+export const useAuth = () => {
+	const dispatch = useAppDispatch()
+
+	const user = useAppSelector(selectUser)
+	const isLoggedIn = Boolean(user)
+
+	const [triggerSignInQuery] = useLazySignInQuery()
+
+	const signIn = async (values: SignInOutDTO['user']) => {
+		const { data } = await triggerSignInQuery(values, false)
+
+		if (!data) {
+			throw new Error('No data in query')
+		}
+
+		dispatch(setUser(data.user))
+	}
+
+	const [triggerSignUpQuery] = useLazySignUpQuery()
+	const signUp = async (values: SignUpOutDTO['user']) => {
+		const { data } = await triggerSignUpQuery(values, false)
+
+		if (!data) {
+			throw new Error('No data in query')
+		}
+
+		dispatch(setUser(data.user))
+	}
+
+	const logOut = () => {
+		dispatch(setUser(null))
+	}
+
+	return {
+		isLoggedIn,
+		signIn,
+		signUp,
+		logOut,
+		user,
+	}
+}
